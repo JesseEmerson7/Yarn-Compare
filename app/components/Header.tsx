c8cb7aaf974f519c8f6ee30d20ef30896f99e598
Jesse Emerson <jesseemerson7@gmail.com>
2024-12-22 22:14:00 -0500

    header svg added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,34 @@
 import Link from "next/link";
+import "./css/header.css";
 
-export default function Header(){
-    return(
-        <header>
-            <img src="/images/logo.png" alt="yarn Compare Logo" height={100} width={100} />
-            <Link href={"/"}>Allie's Yarn Compare</Link>
-            <ul>
-                <li><button>Profile</button></li>
-                <li><button>options</button></li>
-            </ul>
-        </header>
-        
-    );
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <Link id="title" href={"/"}>
+          <img id="logo" src="/images/logo.png" alt="yarn Compare Logo" />
+        </Link>
+        <Link id="title" href={"/"}>
+          Allie's Yarn Compare
+        </Link>
+      </div>
+
+      <ul id="nav">
+        <li className="navOption">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+            </svg>
+          </button>
+        </li>
+        <li className="navOption">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
 }
